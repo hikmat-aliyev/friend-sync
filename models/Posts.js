8a86023c05debb5c2cebaea85e4e0c6f582ced59
Hikmat Ali <hikmat.aliyevm@gmail.com>
2023-12-21 19:27:34 +0400

    Create Posts model

--- models/Posts.js
+++ models/Posts.js
@@ -3,14 +3,15 @@ const Schema = mongoose.Schema;
 const { DateTime } = require("luxon");
 
 const PostSchema = new Schema({
-  text: {type: String, required: true, maxLength: 100},
+  user: { type: Schema.Types.ObjectId, ref: "User", required: true },
+  text: { type: String, required: true, maxLength: 100 },
   date: { type: Date, default: Date.now() },
 });
 
 // Virtual for User's URL
 PostSchema.virtual("url").get(function () {
   // We don't use an arrow function as we'll need the this object
-  return `/User/${this._id}`;
+  return `/User/${this.user}/Post/${this._id}`;
 });
 
 PostSchema.virtual("formatted_date").get(function () {
--- routers/userController.js
+++ routers/userController.js
@@ -2,11 +2,16 @@ const express = require('express');
 const router = express.Router();
 const asyncHandler = require('express-async-handler');
 const { body, validationResult } = require('express-validator');
-const Posts = require('../models/Posts')
+const Post = require('../models/Posts')
 
-router.get('/', (req, res) => {
-  const user = req.user;
-  res.render('user_homepage', {user})
+router.get('/', async (req, res) => {
+  try {
+    const user = req.user;
+    const posts = await Post.find({ user: user._id }).exec();
+    res.render('user_homepage', { user, errors: [], posts });
+  } catch (error) {
+    res.status(500).send('Internal Server Error');
+  }
 });
 
 const handlePostShare = [
@@ -18,10 +23,28 @@ const handlePostShare = [
     .isLength({max: 200})
     .escape()
     .withMessage('Text should not be more than 200 characters.'),
+    
+    asyncHandler(async (req, res, next) => {
+      // Extract the validation errors from a request.
+      const errors = validationResult(req);
+
+      try{
+        if(!errors.isEmpty()){
+          res.render('user_homepage', {errors: errors.array()});
+          return;
+        }
+        const post = new Post({
+          text: req.body.post_text,
+          user: req.user._id
+        })
+        await post.save();
+        res.redirect('/user-homepage');
+      }catch(err){
+        next(err);
+      }
+    })
 ]
 
-router.post('/post', (req, res) => {
-  
-})
+router.post('/post', handlePostShare)
 
 module.exports = router;
